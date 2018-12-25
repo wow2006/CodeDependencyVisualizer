0578e5425b4cb7f0961264d726c149e98c4e7504
Ahmed Hussein <thelastrealdevil@gmail.com>
2018-12-25 22:19:11 +0200

    parse simple json

--- ParseCode/ParseSourceCode.py
+++ ParseCode/ParseSourceCode.py
@@ -1,4 +1,15 @@
+from json import load
+
+
 def ParseSourceCode(json):
-    with open(json, "r") as f:
-        pass
+    with open(json, "r") as json_file:
+        data = load(json_file)
+
+    key   = data[0]["file"]
+    value = data[0]["command"]
+
+    commands = dict()
+    commands[key] = value
+
+    return commands
 
--- tests/test_parse_json.py
+++ tests/test_parse_json.py
@@ -1,5 +1,6 @@
 import unittest
 from context import ParseSourceCode
+from json.decoder import JSONDecodeError
 
 
 class ParseJSON(unittest.TestCase):
@@ -14,6 +15,39 @@ class ParseJSON(unittest.TestCase):
            self.fail('Unexpected exception raised:', e)
         else:
            self.fail('ExpectedException not raised')
+
+    def test_file_empty(self):
+        empty_file = "/tmp/empty.json"
+        with open(empty_file, "w") as f:
+            f.write("")
+
+        try:
+            parser = ParseSourceCode(json=empty_file)
+        except JSONDecodeError as e:
+            self.assertTrue(True)
+        except Exception as e:
+           self.fail('Unexpected exception raised:', e)
+        else:
+           self.fail('ExpectedException not raised')
+
+    def test_simple_file(self):
+        test_file         = "/tmp/test_file.json"
+        test_command_file = "FullSystem.cpp"
+        test_command_dir  = "/tmp"
+        test_command      = "/usr/bin/clang++ %s" % test_command_file
+
+        test_string = '[{"directory": "%s","command": "%s","file": "%s"}]' % (test_command_dir, test_command, test_command_file)
+
+        with open(test_file, "w") as f:
+            f.write(test_string)
+
+        commands = ParseSourceCode(json=test_file)
+
+        self.assertEqual(type(commands), dict)
+        self.assertEqual(len(commands), 1)
+        self.assertIn(test_command_file, commands)
+        self.assertEqual(test_command, commands[test_command_file])
+
 
 if __name__ == '__main__':
     unittest.main()
