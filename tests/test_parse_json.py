730a9c4d66904049430d7a11041ca335a433c6ce
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2018-12-25 19:08:27 +0200

    Fix test

--- tests/test_parse_json.py
+++ tests/test_parse_json.py
@@ -6,8 +6,8 @@ class ParseJSON(unittest.TestCase):
     def test_file_exists(self):
         json_file_directory = "/not/exist.json"
 
-        #with self.assertAlmostEquals(TypeError):
-        parser = ParseSourceCode
+        with self.assertAlmostEquals(TypeError):
+            parser = ParseSourceCode(json=json_file_directory)
 
 
 if __name__ == '__main__':
